5661112d22070ef52fc71a102f70c44b03c6e4ef
uliedberg <ulf@alfhild.io>
2015-11-07 16:07:30 +0100

    add more proper case body

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 const cases = Array.prototype.slice.call(document.querySelectorAll('.case'));
 cases.forEach((el) => {
   el.querySelector('.expand-case').addEventListener('click', onToggleCaseExpansion(el), false);
+  el.querySelector('.collapse-case').addEventListener('click', onToggleCaseCollapse(el), false);
 });
 
 // OK so this is silly
@@ -13,7 +14,20 @@ function onToggleCaseExpansion(caseEl) {
     e.preventDefault();
     caseEl.classList.toggle('collapsed'); // too big scope?
     caseEl.classList.toggle('expanded'); // if we need it
-    caseEl.querySelector('i.fa').classList.toggle('fa-arrow-up');
-    caseEl.querySelector('i.fa').classList.toggle('fa-arrow-circle-down');
+    caseEl.querySelector('.expand-case i.fa').classList.toggle('fa-arrow-up');
+    caseEl.querySelector('.expand-case i.fa').classList.toggle('fa-arrow-circle-down');
+  }
+}
+
+// ah - even sillier
+function onToggleCaseCollapse(caseEl) {
+  return function onToggle(e) {
+    console.log('onToggleCaseCollapse for: ', e, caseEl);
+    onToggleCaseExpansion(caseEl)(e);
+
+    // and yeah - we need a prettier scroll - but that goes for all animation-ish thing
+
+    const boundRect = caseEl.getBoundingClientRect();
+    if (boundRect.bottom < boundRect.height/3) caseEl.scrollIntoView();
   }
 }
