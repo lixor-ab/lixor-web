47518b94b7bde885ba9572120172ddaa586775b4
uliedberg <ulf@alfhild.io>
2017-01-08 00:06:41 +0100

    add debugging info

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,8 @@ DIST_DIR="public"
 
 if [ -n "$TRAVIS_BUILD_ID" ]; then
 
+  echo "we are in travis land"
+
   # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
   if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
