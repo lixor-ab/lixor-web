a443fb15a0742da1e04b960009bd9e2e4a20c6ae
uliedberg <ulf@alfhild.io>
2017-02-15 21:34:32 +0100

    add image minification - super inelegant

---
+++ scripts/minify-images.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const imagemin = require('imagemin');
+const recompress = require('imagemin-jpeg-recompress');
+const svgo = require('imagemin-svgo');
+
+ // TODO: take args
+imagemin(['static/media/**/*.{jpg,svg}'], 'build/', {
+    plugins: [
+        recompress({
+          method: 'smallfry',
+          quality: 'medium',
+          max: 85
+          // accurate: true
+        }),
+        svgo()
+    ]
+}).then(files => {
+    console.log(files);
+});
