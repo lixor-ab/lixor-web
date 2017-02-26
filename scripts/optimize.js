026daf73d681ddaa04650c97f33f0e09ff991559
uliedberg <ulf@alfhild.io>
2017-02-26 01:04:52 +0100

    add image optimize to build instead of one off

--- scripts/optimize.js
+++ scripts/optimize.js
@@ -7,17 +7,28 @@ const gulp = require('gulp');
 const rev = require('gulp-rev');
 const revReplace = require('gulp-rev-replace');
 const gulpif = require('gulp-if');
+const imagemin = require('gulp-imagemin');
+const recompress = require('imagemin-jpeg-recompress');
+const svgo = require('imagemin-svgo');
+const lazypipe = require('lazypipe');
 
 
 const args = process.argv.slice(2);
 const inDir = args[0];
 const outDir = args[1];
 
-const assetLeaf = 'media/**/*.{svg,jpg,jpeg,png}';
+const assetMedia = 'media/**/*.{svg,jpg,jpeg,png}';
 const assetMiddle = '**/*.{css,js}';
 
+const mediaPipeline = lazypipe()
+  .pipe(imagemin, [
+    recompress({ method: 'smallfry', quality: 'medium', max: 85 }),
+    svgo()
+  ])
+  .pipe(rev);
+
 gulp.src(`${inDir}/**/*`, { base: inDir})
-  .pipe(gulpif(assetLeaf, rev()))
+  .pipe(gulpif(assetMedia, mediaPipeline()))
   .pipe(revReplace())
   .pipe(gulpif(assetMiddle, rev()))
   .pipe(revReplace())
