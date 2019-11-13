675528b623a25db10180afd385dbf1a2652ad5d5
Ulf Liedberg <ulf@liedberg.org>
2019-11-13 21:03:24 +0100

    Update s3-deploy.sh

--- scripts/s3-deploy.sh
+++ scripts/s3-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-aws s3 sync public s3://$S3_BUCKET --region eu-central-1 --exclude 'assets/*' --delete
-aws s3 sync public/assets s3://$S3_BUCKET/assets --region eu-central-1 --cache-control "max-age=31536000" --delete
+# no more aws :)
+#aws s3 sync public s3://$S3_BUCKET --region eu-central-1 --exclude 'assets/*' --delete
+#aws s3 sync public/assets s3://$S3_BUCKET/assets --region eu-central-1 --cache-control "max-age=31536000" --delete
