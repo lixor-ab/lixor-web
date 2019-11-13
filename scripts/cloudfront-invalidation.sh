8c4c799367b9308a2e154a5bc4f4768e06b913d1
Ulf Liedberg <ulf@liedberg.org>
2019-11-13 21:03:52 +0100

    Update cloudfront-invalidation.sh

--- scripts/cloudfront-invalidation.sh
+++ scripts/cloudfront-invalidation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
+# no more aws :)
+#aws configure set preview.cloudfront true
+#aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
